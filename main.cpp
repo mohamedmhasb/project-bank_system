a5e9a99e4ea8a876a9b844532267b503dfe510ae
Mohamed m hasb <mohamedmhasb362@gmail.com>
2025-09-07 06:23:34 +0300

    frist change

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@ using namespace std;
 int main()
 {
 
-Client c( 1,"Ahmheed", "jk", 3000);
+Client c( 1,"Ahmheed", "jkbght", 3000);
 c.displayFun();
-Client p(2, "mohamed", "ggo", 2000);
+Client p(2, "mohamed", "ggohth4", 2000);
 
 p.displayFun();
 c.transfer_to(2000, p);
 c.displayFun();
 p.displayFun();
-
-
+Client b(3,"Ahmed Maher","kfdkg4652",10000);
+b.displayFun();
     return 0;
 }
